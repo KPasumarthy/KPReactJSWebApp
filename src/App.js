08b029350aaae38766bd33d2de6436fc414c1281
Kailash Pasumarthy <KPasumarthy@gmail.com>
2021-03-08 23:51:09 -0500

    KP : ReactJS App - Additional Components Added!

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+import AppZero from './appZero/AppZero';
+import Button from './components/Button';
+
+const App = () => {
+  const [showAppZero, setShowAppZero] = useState(true);
+  const [showAddTask, setShowAddTask, ] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      name: 'this 1 task',
+    },
+    {
+      id: 2,
+      name: 'this 2 task',
+    },
+    {
+      id: 3,
+      name: 'this 3 task',
+    }
+  ])
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task }
+    setTasks(...tasks, newTask);
+  }
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+
+  ////KP : showAppZero = true  : Renders AppZero! showAppZero = false  : Renders Tasks App
+  if (showAppZero) {
+    return (
+      <div>
+        <AppZero />
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="container">
+        <Header onAdd={() => { setShowAddTask(!showAddTask) }} showAdd={showAddTask} />
+        {showAddTask && <AddTask onAdd={addTask} />}
+        {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show!')}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/appZero/AppZero.js
+++ src/appZero/AppZero.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './AppZero.css';
 
-class App extends Component {
 
-  constructor(props){
+
+class AppZero extends Component {
+
+  constructor(props) {
     super(props);
     this.state = {
       items: [],
@@ -16,13 +18,13 @@ class App extends Component {
   componentDidMount() {
 
     fetch('http://localhost:2727/mongodbnosqlapi/customersAgeSorted')
-    .then(res => res.json())
-    .then(json => {
-      this.setState({
-        isLoaded: true,
-        items: json,
-      })
-    });
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
 
   }
 
@@ -30,19 +32,20 @@ class App extends Component {
 
     //KP : React Class-level variables
     const name = 'Kailash';
-    const element = <h1>Hello World : {name} - ReactJS</h1> 
+    const element = <h1>Hello World : {name} - ReactJS</h1>
+
 
     //KP : Items from API calls
     var { isLoaded, items } = this.state;
 
-    if (!isLoaded){
+    if (!isLoaded) {
       return (
-        <div className="App">        
-          <header className="App-header">
+        <div className="container">
+          <header className="header">
             {element}
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="logo" alt="logo" />
             <p>
-              Edit <code>src/App.js</code> and save to reload.
+              Edit <code>src/AppZero.js</code> and save to reload.
               LOADING ....
             </p>
             <a
@@ -54,19 +57,20 @@ class App extends Component {
               Learn React
             </a>
           </header>
+
         </div>
       );
-    }else{
+    } else {
       return (
-        <div className="App">        
-          <header className="App-header">
+        <div className="AppZero">
+          <header className="AppZero-header">
             {element}
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="AppZero-logo" alt="logo" />
             <p>
-              Edit <code>src/App.js</code> and save to reload.
+              Edit <code>src/AppZero.js</code> and save to reload.
             </p>
             <a
-              className="App-link"
+              className="AppZero-link"
               href="https://reactjs.org"
               target="_blank"
               rel="noopener noreferrer"
@@ -79,7 +83,7 @@ class App extends Component {
               {
                 items.map(item => (
                   <li key={item._id}>
-                      FirstName: {item.first_name} | LastName: {item.last_name}
+                    FirstName: {item.first_name} | LastName: {item.last_name}
                   </li>
                 ))
               }
@@ -94,4 +98,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default AppZero;
